730298e7d869fc634733c7f145e51c44b384b75c
srikarn <srikarnanduri628@gmail.com>
2018-03-20 10:51:09 +0530

    Trying to share image from a URL

--- app/src/main/java/com/example/android/intentsexample/IntentListAdapter.java
+++ app/src/main/java/com/example/android/intentsexample/IntentListAdapter.java
@@ -1,16 +1,29 @@
 package com.example.android.intentsexample;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v4.app.ShareCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -41,7 +54,7 @@ public class IntentListAdapter extends
     }
 
     @Override
-    public void onBindViewHolder(final IntentViewHolder holder, int position) {
+    public void onBindViewHolder(final IntentViewHolder holder,final int position) {
         holder.mLoadingIndicator.setVisibility(View.VISIBLE);
         Picasso.with(context).load(mImageButtons.get(position).getAndroid_image_url())
                 .resize(800, 500)
@@ -56,6 +69,14 @@ public class IntentListAdapter extends
 
                     }
                 });
+        holder.mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shareItem(mImageButtons.get(position).getAndroid_image_url());
+            }
+        });
+
+
     }
 
     @Override
@@ -66,6 +87,7 @@ public class IntentListAdapter extends
     class IntentViewHolder extends RecyclerView.ViewHolder{
 
         public final ImageButton mImageButton;
+        public final Button mButton;
         public  final IntentListAdapter mAdapter;
         private ProgressBar mLoadingIndicator;
         public IntentViewHolder(View itemView, IntentListAdapter adapter) {
@@ -73,7 +95,37 @@ public class IntentListAdapter extends
             this.mAdapter = adapter;
             mLoadingIndicator =  itemView.findViewById(R.id.pb_loading_indicator);
             mImageButton = itemView.findViewById(R.id.imageButton);
-
+            mButton = itemView.findViewById(R.id.share);
         }
     }
+
+
+
+    public void shareItem(String url) {
+        Picasso.with(context).load(url).into(new Target() {
+            @Override public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                Intent i = new Intent( Intent.ACTION_SEND);
+                i.setType("image/*");
+                i.putExtra(Intent.EXTRA_STREAM, getLocalBitmapUri(bitmap));
+               context.startActivity(Intent.createChooser(i, "Share Image"));
+            }
+            @Override public void onBitmapFailed(Drawable errorDrawable) { }
+            @Override public void onPrepareLoad(Drawable placeHolderDrawable) { }
+        });
+    }
+
+    public Uri getLocalBitmapUri(Bitmap bmp) {
+        Uri bmpUri = null;
+        try {
+            File file =  new File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "share_image_" + System.currentTimeMillis() + ".png");
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
+    }
+
 }
